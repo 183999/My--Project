028e891bd2df03b82c114f477a7a5239d945e37d
183999 <68915454+183999@users.noreply.github.com>
2020-08-03 12:29:53 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,8 @@
+@@ -1,7 +1,3 @@
+
+
+
+
 function gcheck{
         echo "guess number of files:"
         read count
-      num file =$(ls -l | wc - l)
-}
-
-echo "Welcome to the Game"
-
-gcheck
-
-while [[ $count  -ne $num file ]]
-do
-      if [[$count -gt $num files ]]
-      then
-             echo " sorry.Your guess is high "
-     else        
-            echo "sorry. Your guess is small."
-     fi
-     gcheck
-done
-
-echo " correct answer."
-echo "Thank you for Participation !!!"
